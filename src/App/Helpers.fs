1340f70f4dad330b2da1b40fa0cce1c265ad3144
Miroslaw Puczynski <public.mila@gmail.com>
2018-12-04 08:57:33 +0100

    add day 2

--- src/App/Helpers.fs
+++ src/App/Helpers.fs
@@ -1,6 +1,7 @@
 ﻿module Helpers
 
 open System.IO
+open System.Text.RegularExpressions
 
 let readLinesFromFile (filePath:string) = seq {
     use sr = new StreamReader (filePath)
@@ -10,5 +11,19 @@ let readLinesFromFile (filePath:string) = seq {
 
 let readLines(s: string) =
     s.Split [| '\n' |] 
+
+let parseLines (s: string) =
+    s.Split([|'\r'; '\n'|])
+   |> Array.choose (fun row ->
+        match row.Trim() with
+        | "" -> None
+        | "\n" -> None
+        | row -> Some row)
+    |> Seq.ofArray
+
+let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
 
 
--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,12 +1,11 @@
 ﻿module App.Main
 
-    open App.Day1
-
     [<EntryPoint>]
     let main argv =
         let out = match argv with
                     | [|day|] -> match day with
                                     | "1" -> sprintf "%A" <| Day1.part2()
+                                    | "2" -> sprintf "%A" <| Day2.part2()
                                     | _ -> ""
                     | _ -> "Insert number of day as parameter" 
         printfn "%s" out
