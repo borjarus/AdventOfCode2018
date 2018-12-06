f1dbe247720b619a0c85bdc4b8c71ec5589584a7
Miroslaw Puczynski <public.mila@gmail.com>
2018-12-06 23:00:57 +0100

    day 6 - partial

--- src/App/Day6.fs
+++ src/App/Day6.fs
@@ -53,10 +53,96 @@ What is the size of the largest area that isn't infinite?
 
 module App.Day6
 open Helpers
+open System
+
+    let parseLine line = 
+        match line with 
+        | Regex @"(\d+), (\d+)" [d1; d2] -> (int32 d1, int32 d2)
+        | _ -> failwith "parsing error"
+    
+    let maxX parsedInput = 
+        parsedInput |> Seq.map fst |> Seq.reduce max |> int
+
+    let maxY parsedInput = 
+        parsedInput |> Seq.map snd |> Seq.reduce max |> int 
+
+
+    
+    let manhattan (x:int32, y: int32) (x': int32, y': int32) =
+         (Math.Abs (x - x')) + (Math.Abs (y - y'))
+    
+    let closestTo  inp (x,y) =
+        let closest =  
+            inp |> Seq.map (fun (x',y') -> 
+                (manhattan (x,y) (x',y'), (x',y'))) |> Seq.sortBy fst
+        if (closest |> Seq.item 0 |> fst) <> (closest |> Seq.item 1 |> fst)
+        then Some ((closest |> Seq.item 0 |> snd), (x,y))
+        else None
+    
 
     let examples1() =
-        let input = "dabAcCaCBAcCcaDA"
-        ()
+        let input = 
+            parseLines "1, 1\n
+1, 6\n
+8, 3\n
+3, 4\n
+5, 5\n
+8, 9\n"
+        let parsedInput = 
+            input
+            |> Seq.map parseLine
+        
+        
+        
+        let maxX = maxX parsedInput
+        let maxY = maxY parsedInput
+
+        let coordinates =
+            seq {
+                for x in [0.. maxX] do
+                    for y in [0..maxY] do 
+                    yield x, y}
+
+        let isInfiniteArea area =
+            area 
+            |> Seq.exists (fun (x,y) ->
+                if x = 0 || y = 0 || x >= maxX || y >= maxY 
+                then true
+                else false
+            )
+
+
+        let largestAreaSize =
+            let groups = 
+                coordinates
+                |> Seq.filter (fun inp -> 
+                    let x = inp |> closestTo parsedInput
+                    match x with 
+                    | Some _ -> true
+                    | None -> false)
+                |> Seq.groupBy fst 
+            groups
+            |> Seq.map snd
+            |> Seq.map (fun inp ->
+            if not (isInfiniteArea inp)
+            then Some (Seq.length inp)
+            else None)
+            //|> Seq.filter (fun tup -> 
+            //    if not (isInfiniteArea tup)
+            //    then true
+            //    else false
+            //)
+
+//(defn solve-1 []
+//  (let [groups (group-by first (keep closest-to coordinates))]
+//    (apply max (keep #(when (not (infinite-area? (map second %)))
+//        (count %)) (vals groups)))))
+
+
+        largestAreaSize |> Seq.toList
+
+
+
 
 
 
