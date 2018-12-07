059691a1f5ee77a8b42d0d0d766beeca9de5acc2
Miroslaw Puczynski <public.mila@gmail.com>
2018-12-07 11:30:22 +0100

    finished day6

--- src/App/Day6.fs
+++ src/App/Day6.fs
@@ -3,13 +3,17 @@
 --- Day 6: Chronal Coordinates ---
 The device on your wrist beeps several times, and once again you feel like you're falling.
 
-"Situation critical," the device announces. "Destination indeterminate. Chronal interference detected. Please specify new target coordinates."
+"Situation critical," the device announces. "Destination indeterminate. Chronal interference detected. 
+Please specify new target coordinates."
 
-The device then produces a list of coordinates (your puzzle input). Are they places it thinks are safe or dangerous? It recommends you check manual page 729. The Elves did not give you a manual.
+The device then produces a list of coordinates (your puzzle input). Are they places it thinks are safe or dangerous? 
+It recommends you check manual page 729. The Elves did not give you a manual.
 
-If they're dangerous, maybe you can minimize the danger by finding the coordinate that gives the largest distance from the other points.
+If they're dangerous, maybe you can minimize the danger by finding the coordinate that gives the largest distance 
+from the other points.
 
-Using only the Manhattan distance, determine the area around each coordinate by counting the number of integer X,Y locations that are closest to that coordinate (and aren't tied in distance to any other coordinate).
+Using only the Manhattan distance, determine the area around each coordinate by counting the 
+number of integer X,Y locations that are closest to that coordinate (and aren't tied in distance to any other coordinate).
 
 Your goal is to find the size of the largest area that isn't infinite. For example, consider the following list of coordinates:
 
@@ -31,7 +35,8 @@ If we name these coordinates A through F, we can draw them on a grid, putting 0,
 ..........
 ..........
 ........F.
-This view is partial - the actual grid extends infinitely in all directions. Using the Manhattan distance, each location's closest coordinate can be determined, shown here in lowercase:
+This view is partial - the actual grid extends infinitely in all directions. Using the Manhattan distance, 
+each location's closest coordinate can be determined, shown here in lowercase:
 
 aaaaa.cccc
 aAaaa.cccc
@@ -45,9 +50,53 @@ bbb.eeffff
 bbb.ffffFf
 Locations shown as . are equally far from two or more coordinates, and so they don't count as being closest to any.
 
-In this example, the areas of coordinates A, B, C, and F are infinite - while not shown here, their areas extend forever outside the visible grid. However, the areas of coordinates D and E are finite: D is closest to 9 locations, and E is closest to 17 (both including the coordinate's location itself). Therefore, in this example, the size of the largest area is 17.
+In this example, the areas of coordinates A, B, C, and F are infinite - while not shown here, their areas extend 
+forever outside the visible grid. However, the areas of coordinates D and E are finite: D is closest to 9 locations, 
+and E is closest to 17 (both including the coordinate's location itself). Therefore, in this example, 
+the size of the largest area is 17.
 
 What is the size of the largest area that isn't infinite?
+
+
+--- Part Two ---
+On the other hand, if the coordinates are safe, maybe the best you can do is try to find a region near as 
+many coordinates as possible.
+
+For example, suppose you want the sum of the Manhattan distance to all of the coordinates to be less than 32. 
+For each location, add up the distances to all of the given coordinates; if the total of those distances 
+is less than 32, that location is within the desired region. Using the same coordinates as above, 
+the resulting region looks like this:
+
+..........
+.A........
+..........
+...###..C.
+..#D###...
+..###E#...
+.B.###....
+..........
+..........
+........F.
+In particular, consider the highlighted location 4,3 located at the top middle of the region. 
+Its calculation is as follows, where abs() is the absolute value function:
+
+Distance to coordinate A: abs(4-1) + abs(3-1) =  5
+Distance to coordinate B: abs(4-1) + abs(3-6) =  6
+Distance to coordinate C: abs(4-8) + abs(3-3) =  4
+Distance to coordinate D: abs(4-3) + abs(3-4) =  2
+Distance to coordinate E: abs(4-5) + abs(3-5) =  3
+Distance to coordinate F: abs(4-8) + abs(3-9) = 10
+Total distance: 5 + 6 + 4 + 2 + 3 + 10 = 30
+Because the total distance to all coordinates (30) is less than 32, the location is within the region.
+
+This region, which also includes coordinates D and E, has a total size of 16.
+
+Your actual region will need to be much larger than this example, though, instead including 
+all locations with a total distance of less than 10000.
+
+What is the size of the region containing all locations which have a total distance 
+to all given coordinates of less than 10000?
+
 
 *)
 
@@ -115,31 +164,19 @@ open System
         let largestAreaSize =
             let groups = 
                 coordinates
-                |> Seq.filter (fun inp -> 
-                    let x = inp |> closestTo parsedInput
-                    match x with 
-                    | Some _ -> true
-                    | None -> false)
+                |> Seq.choose (closestTo parsedInput)
                 |> Seq.groupBy fst 
             groups
             |> Seq.map snd
-            |> Seq.map (fun inp ->
-            if not (isInfiniteArea inp)
-            then Some (Seq.length inp)
-            else None)
-            //|> Seq.filter (fun tup -> 
-            //    if not (isInfiniteArea tup)
-            //    then true
-            //    else false
-            //)
+            |> Seq.choose (fun el -> 
+                let second = el |> Seq.map  snd
+                if not (isInfiniteArea second)
+                then Some (Seq.length el)
+                else None
+            )
+            |> Seq.reduce max
 
-//(defn solve-1 []
-//  (let [groups (group-by first (keep closest-to coordinates))]
-//    (apply max (keep #(when (not (infinite-area? (map second %)))
-//        (count %)) (vals groups)))))
-
-
-        largestAreaSize |> Seq.toList
+        largestAreaSize
 
 
 
@@ -147,14 +184,121 @@ open System
 
 
     let examples2() = 
-        let input = "dabAcCaCBAcCcaDA"
-        ()
+        let input = 
+            parseLines "1, 1\n
+1, 6\n
+8, 3\n
+3, 4\n
+5, 5\n
+8, 9\n"
+        
+        let parsedInput = 
+            input
+            |> Seq.map parseLine
+
+        let maxX = maxX parsedInput
+        let maxY = maxY parsedInput
+
+        let coordinates =
+            seq {
+                for x in [0.. maxX] do
+                    for y in [0..maxY] do 
+                    yield x, y}
+
+        let manhattanFn =
+            parsedInput
+            |> Seq.map manhattan
+
+
+        coordinates
+        |> Seq.map (fun el ->
+                manhattanFn
+                |> Seq.map (fun fn ->
+                    el |> fn
+                ) 
+                |> Seq.sum
+        ) 
+        |> Seq.filter (fun el ->
+            el < 32
+        )
+        |> Seq.length
+
 
 
     let part1() = 
-        let input = readLinesFromFile(@"day6.txt") |> Seq.head
-        ()
+        let input = readLinesFromFile(@"day6.txt")
+        let parsedInput = 
+            input
+            |> Seq.map parseLine
+        
+        
+        
+        let maxX = maxX parsedInput
+        let maxY = maxY parsedInput
+
+        let coordinates =
+            seq {
+                for x in [0.. maxX] do
+                    for y in [0..maxY] do 
+                    yield x, y}
+
+        let isInfiniteArea area =
+            area 
+            |> Seq.exists (fun (x,y) ->
+                if x = 0 || y = 0 || x >= maxX || y >= maxY 
+                then true
+                else false
+            )
+
+
+        let largestAreaSize =
+            let groups = 
+                coordinates
+                |> Seq.choose (closestTo parsedInput)
+                |> Seq.groupBy fst 
+            groups
+            |> Seq.map snd
+            |> Seq.choose (fun el -> 
+                let second = el |> Seq.map  snd
+                if not (isInfiniteArea second)
+                then Some (Seq.length el)
+                else None
+            )
+            |> Seq.reduce max
+
+        largestAreaSize
+
 
     let part2() = 
-        let input = readLinesFromFile(@"day6.txt") |> Seq.head
-        ()
+        let input = readLinesFromFile(@"day6.txt")
+        
+        let parsedInput = 
+            input
+            |> Seq.map parseLine
+
+        let maxX = maxX parsedInput
+        let maxY = maxY parsedInput
+
+        let coordinates =
+            seq {
+                for x in [0.. maxX] do
+                    for y in [0..maxY] do 
+                    yield x, y}
+
+        let manhattanFn =
+            parsedInput
+            |> Seq.map manhattan
+
+
+        coordinates
+        |> Seq.map (fun el ->
+                manhattanFn
+                |> Seq.map (fun fn ->
+                    el |> fn
+                )
+                |> Seq.sum
+        ) 
+        |> Seq.filter (fun el ->
+            el < 10000
+        )
+        |> Seq.length
