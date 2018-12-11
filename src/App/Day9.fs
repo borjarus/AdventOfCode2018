7df4b52b8911ab7485d957754cb3f8117693c0aa
Miroslaw Puczynski <public.mila@gmail.com>
2018-12-11 16:26:21 +0100

    day9 WIP

--- src/App/Day9.fs
+++ src/App/Day9.fs
@@ -72,16 +72,19 @@ What is the winning Elf's score?
 module App.Day9
 open Helpers
 open System.Collections.Generic
+open System.Runtime.Serialization
 
+    type Circle = {Before: int list; Current: int; After: int list}
 
     type GameArena = {
         Players: int
         Player: int
-        Index: int
+        //Index: int
         Marble: int
-        Circle:  int list
+        Circle:  Circle
         Scores: Map<int,int>
     }
+
 
     //let counterClockwiseIndex circle index n =
     //    let rsubCircle = circle.Keys |> List.ofSeq |> List.filter ((<=) index) |> List.rev
@@ -93,52 +96,91 @@ open System.Collections.Generic
         then index + 2
         else 2
 
-    let makeNewCircle (circle: int list) index marble =
-        if index <= circle.Length
-        then 
-            let prev = circle |> List.take (index - 1)
-            let next = circle |> List.skip (index - 1)
-            prev @ [marble] @ next
-        else 
-            circle @ [marble]
+    //let makeNewCircle (circle: int list) index marble =
+    //    if index <= circle.Length
+    //    then 
+    //        let prev = circle |> List.take (index - 1)
+    //        let next = circle |> List.skip (index - 1)
+    //        prev @ [marble] @ next
+    //    else 
+    //        circle @ [marble]
     
-    let removeFromCircle circle index =
-        let prev = circle |> List.take (index - 1)
-        let h::next = circle |> List.skip (index - 1)
-        prev @ next, h
+    //let removeFromCircle circle index =
+    //    let prev = circle |> List.take (index - 1)
+    //    let h::next = circle |> List.skip (index - 1)
+    //    prev @ next, h
 
 
     let newGame players =
         {
             Players= players
-            Player= 1
-            Index= 2
-            Marble= 1
-            Circle= [0;1]
+            Player= 0
+            //Index= 2
+            Marble= 0
+            Circle= {Before=[]; Current= 0; After=[]}
             Scores= Map.empty
         }
+    
+    let rotateCounterClockwise {Before=before; Current=curr; After=after} =
+        match before with
+        | [] ->
+            let x::xs = List.rev (curr::after)
+            {Before=xs; Current= x; After=[]}
+        | x :: xs -> {Before= xs; Current=x; After=curr::after}
+    
+    let rotateClockwise {Before=before; Current=curr; After=after} =
+        match after with
+        | [] ->
+            let x::xs = List.rev (curr::before)
+            {Before=[]; Current= x; After= xs}
+        | x::xs -> {Before= curr::before; Current= x; After= xs}
+    
+    let rec rotate n circle =
+        match n with
+        | 0 -> circle
+        | _ when n > 0 -> circle |> rotateClockwise |> rotate (n - 1)
+        | _ -> circle |> rotateCounterClockwise |> rotate (n + 1)
+    
+    let insertMarble marble circle = {circle with Before= circle.Current::circle.Before; Current= marble} 
+
+    let removeCurrent {Before=before; After=after} =
+        match after with 
+        | [] -> {Before= List.tail before; Current= List.head before; After=after} |> rotate 1
+        | x::xs -> {Before=before; Current= x; After= xs}
 
     let advance game nth =
         let rec nextAdvance game =
-            let {Players= players; Player= player; Marble= marble; Index=index; Circle= circle; Scores= score} = game
-            if marble = nth then game
-            else
-                let game = {game with Marble= marble + 1; Player= (player % players) + 1}
-                if (marble + 1) % 23 = 0 
-                then
-                    let newIndex, newIndex' = if (game.Index - 7) <= 0 then circle.Length + (game.Index - 7), circle.Length + (game.Index - 7)   else (game.Index - 7), (game.Index - 7)
-                    let newCircle, removedValue = removeFromCircle circle newIndex
-                    let newScore = 
-                        match score.TryFind(player) with
-                        | Some s -> (game.Marble + removedValue) + s
-                        | None -> (game.Marble + removedValue)
-                    
-                    nextAdvance {game with Index= newIndex'; Circle= newCircle; Scores= score.Add(game.Marble % game.Players, newScore )}
-                else         
-                    let newIndex = placeIndex circle index
-                    nextAdvance {game with Index= newIndex; Circle= makeNewCircle circle newIndex game.Marble}
+            let {Players= players; Player= player; Marble= marble;  Circle= circle; Scores= score} = game
+            if marble <> nth 
+            then 
+                let newMarble = marble + 1
+                let newCircle = game.Circle |> rotate 1 |> insertMarble newMarble
+                nextAdvance {game with Circle= newCircle; Marble= newMarble; Player= (player % players) + 1}
+            else game
         
         nextAdvance game
+
+    //let advance game nth =
+    //    let rec nextAdvance game =
+    //        let {Players= players; Player= player; Marble= marble; Index=index; Circle= circle; Scores= score} = game
+    //        if marble = nth then game
+    //        else
+    //            let game = {game with Marble= marble + 1; Player= (player % players) + 1}
+    //            if (marble + 1) % 23 = 0 
+    //            then
+    //                let newIndex, newIndex' = if (game.Index - 7) <= 0 then circle.Length + (game.Index - 7), circle.Length + (game.Index - 7)   else (game.Index - 7), (game.Index - 7)
+    //                let newCircle, removedValue = removeFromCircle circle newIndex
+    //                let newScore = 
+    //                    match score.TryFind(player) with
+    //                    | Some s -> (game.Marble + removedValue) + s
+    //                    | None -> (game.Marble + removedValue)
+                    
+    //                nextAdvance {game with Index= newIndex'; Circle= newCircle; Scores= score.Add(game.Player % game.Players, newScore )}
+    //            else         
+    //                let newIndex = placeIndex circle index
+    //                nextAdvance {game with Index= newIndex; Circle= makeNewCircle circle newIndex game.Marble}
+        
+    //    nextAdvance game
         
 
 
@@ -146,15 +188,17 @@ open System.Collections.Generic
 
     let examples1() =
         let input = parseLines ""
+        advance (newGame 9) 4
+
 
         //nextAdvance {Players= 9; Player= 1; Index= 2; Marble= 1; Circle= [0; 1]; Scores= 0}
         //nextAdvance {Players= 9; Player= 2; Index= 2; Marble= 2; Circle= [0;2;1]; Scores= 0}
         //nextAdvance {Players= 9; Player= 3; Index= 4; Marble= 3; Circle= [0;2;1;3]; Scores= 0}
         //nextAdvance {Players= 9; Player= 4; Index= 2; Marble= 4; Circle= [0; 4; 2; 1; 3]; Scores= 0}
         //nextAdvance {Players= 9; Player= 4; Index= 22; Marble= 23; Circle= [0;16;8;17;4;18;9;19;2;20;10;21;5;22;11;1;12;6;13;3;14;7;15]; Scores= 0}
-        advance (newGame 9) 25
+        //advance (newGame 9) 25
         //let {Scores= score} = advance (newGame 10) 1618 
-        advance (newGame 10) 1618
+        //advance (newGame 10) 1618
         //advance (newGame 13) 7999
 
 
