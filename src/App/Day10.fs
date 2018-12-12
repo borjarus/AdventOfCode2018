6a31d088de894423b5f5318c5b833295ca03a9bf
Miroslaw Puczynski <public.mila@gmail.com>
2018-12-12 11:11:28 +0100

    end day10

--- src/App/Day10.fs
+++ src/App/Day10.fs
@@ -148,9 +148,17 @@ After 4 seconds:
 ......................
 ......................
 
-After 3 seconds, the message appeared briefly: HI. Of course, your message will be much longer and will take many more seconds to appear.
+After 3 seconds, the message appeared briefly: HI. Of course, your message will be much longer and will 
+take many more seconds to appear.
 
 What message will eventually appear in the sky?
+
+--- Part Two ---
+Good thing you didn't have to wait, because that would have taken a long time - much longer than 
+the 3 seconds in the example above.
+
+Impressed by your sub-hour communication capabilities, the Elves are curious: exactly 
+how many seconds would they have needed to wait for that message to appear?
 
 
 *)
@@ -158,7 +166,8 @@ What message will eventually appear in the sky?
 module App.Day10
 open Helpers
 
-    let height = 8
+
+    let height = 10
 
     let iterate sList = 
         sList 
@@ -166,16 +175,48 @@ open Helpers
             | p1, p2, v1, v2 -> (p1 + v1), (p2 + v2), v1, v2
             | _ -> failwith "incorrect list length")
 
-    let rec findGrouped inp = 
+
+    //let rec findGrouped listOfHeights inp   = 
+    //    match iterate inp with
+    //    | out when List.exists (fun el -> 
+    //            (out |> List.groupBy (fun (_ ,y, _, _) -> y) |> List.length) = el) listOfHeights  -> out
+    //    | out -> findGrouped listOfHeights out 
+
+    let rec findGrouped listOfHeights inp   = 
         match iterate inp with
         | out when (out |> List.groupBy (fun (_ ,y, _, _) -> y) |> List.length) = height -> out
-        | out -> findGrouped out
+        | out -> findGrouped listOfHeights out 
 
+    let rec findSeq inp = seq{
+            let inner, out = match iterate inp with
+                | out  -> (out |> List.groupBy (fun (_ ,y, _, _) -> y) |> List.length), out
+            yield inner
+            yield! findSeq out
+            
+        }
 
-    let rec find inp = 
+    let generateStr lst =
+        [| 0 .. (Set.maxElement lst)|]
+        |> Array.map (fun x -> if Set.contains x lst then '#' else '.')
+        |> System.String
+
+    let generateMassage lstOfStars =
+        let minX = lstOfStars |> List.minBy fst |> fst
+        let minY = lstOfStars |> List.minBy fst |> fst
+
+        lstOfStars
+        |> List.map (fun (x,y) -> 
+            (x - minX), (y - minY)
+        )
+        |> List.groupBy snd
+        |> List.sortBy fst
+        |> List.map (fun (_, b) -> b  |> List.map fst |> Set.ofList |> generateStr)
+        |> List.iter (fun str -> printfn "%s" str)
+
+    let rec howManyCount i inp =
         match iterate inp with
-        | out  -> printfn "%d" (out |> List.groupBy (fun (_ ,y, _, _) -> y) |> List.length); find out
-    
+        | out when (out |> List.groupBy (fun (_ ,y, _, _) -> y) |> List.length) = height -> i
+        | out -> howManyCount (i + 1) out 
 
   let parseLine str =
         match str with
@@ -215,25 +256,104 @@ position=< 5,  9> velocity=< 1, -2>\n
 position=<14,  7> velocity=<-2,  0>\n
 position=<-3,  6> velocity=< 2, -1>"
 
+        let parsedInput = 
+            input
+            |> Seq.toList
+            |> List.map parseLine
+
+        let listOfHeights =
+            parsedInput
+            |> findSeq
+            |> Seq.take 150
+            |> Seq.toList
+            |> List.distinct
+            
+        
         input
         |> Seq.toList
         |> List.map parseLine
-        //|> findGrouped
-        |> find
+        |> findGrouped listOfHeights
+        |> List.map (fun (x,y,_,_) -> x,y) 
+        |> generateMassage
+        listOfHeights
+        
+
+
 
         
 
 
 
     let examples2() = 
-        let input = parseLines ""
-        ()
+        let input = parseLines "position=< 9,  1> velocity=< 0,  2>\n
+position=< 7,  0> velocity=<-1,  0>\n
+position=< 3, -2> velocity=<-1,  1>\n
+position=< 6, 10> velocity=<-2, -1>\n
+position=< 2, -4> velocity=< 2,  2>\n
+position=<-6, 10> velocity=< 2, -2>\n
+position=< 1,  8> velocity=< 1, -1>\n
+position=< 1,  7> velocity=< 1,  0>\n
+position=<-3, 11> velocity=< 1, -2>\n
+position=< 7,  6> velocity=<-1, -1>\n
+position=<-2,  3> velocity=< 1,  0>\n
+position=<-4,  3> velocity=< 2,  0>\n
+position=<10, -3> velocity=<-1,  1>\n
+position=< 5, 11> velocity=< 1, -2>\n
+position=< 4,  7> velocity=< 0, -1>\n
+position=< 8, -2> velocity=< 0,  1>\n
+position=<15,  0> velocity=<-2,  0>\n
+position=< 1,  6> velocity=< 1,  0>\n
+position=< 8,  9> velocity=< 0, -1>\n
+position=< 3,  3> velocity=<-1,  1>\n
+position=< 0,  5> velocity=< 0, -1>\n
+position=<-2,  2> velocity=< 2,  0>\n
+position=< 5, -2> velocity=< 1,  2>\n
+position=< 1,  4> velocity=< 2,  1>\n
+position=<-2,  7> velocity=< 2, -2>\n
+position=< 3,  6> velocity=<-1, -1>\n
+position=< 5,  0> velocity=< 1,  0>\n
+position=<-6,  0> velocity=< 2,  0>\n
+position=< 5,  9> velocity=< 1, -2>\n
+position=<14,  7> velocity=<-2,  0>\n
+position=<-3,  6> velocity=< 2, -1>"
+        let parsedInput = 
+            input
+            |> Seq.toList
+            |> List.map parseLine
+
+        parsedInput
+        |> howManyCount 1
 
     let part1() = 
         let input = readLinesFromFile(@"day10.txt")
-        ()
+        let parsedInput = 
+            input
+            |> Seq.toList
+            |> List.map parseLine
+
+        let listOfHeights =
+            parsedInput
+            |> findSeq
+            |> Seq.take 500
+            |> Seq.toList
+            |> List.distinct
+            
+        
+        input
+        |> Seq.toList
+        |> List.map parseLine
+        |> findGrouped listOfHeights
+        |> List.map (fun (x,y,_,_) -> x,y) 
+        |> generateMassage
+        listOfHeights
 
 
     let part2() = 
         let input = readLinesFromFile(@"day10.txt")
-        ()
+        let parsedInput = 
+            input
+            |> Seq.toList
+            |> List.map parseLine
+
+        parsedInput
+        |> howManyCount 1
--- src/App/Program.fs
+++ src/App/Program.fs
@@ -12,8 +12,8 @@
                                     | "6" -> sprintf "%A" <| Day6.part2()
                                     | "7" -> sprintf "%A" <| Day7.part2()
                                     | "8" -> sprintf "%A" <| Day8.part2()
-                                    | "9" -> sprintf "%A" <| Day9.examples1()
-                                    | "10" -> sprintf "%A" <| Day10.examples1()
+                                    | "9" -> sprintf "%A" <| Day9.part2()
+                                    | "10" -> sprintf "%A" <| Day10.part2()
                                     | _ -> ""
                     | _ -> "Insert number of day as parameter" 
         printfn "%s" out
