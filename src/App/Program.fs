944532e729ea6377df1570448511124aaa8bcc01
Miroslaw Puczynski <public.mila@gmail.com>
2018-12-12 14:47:31 +0100

    init day12

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -15,6 +15,7 @@
                                     | "9" -> sprintf "%A" <| Day9.part2()
                                     | "10" -> sprintf "%A" <| Day10.part2()
                                     | "11" -> sprintf "%A" <| Day11.part2()
+                                    | "12" -> sprintf "%A" <| Day12.examples1()
                                     | _ -> ""
                     | _ -> "Insert number of day as parameter" 
         printfn "%s" out
